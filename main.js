e4ffa8cfd051b4451300707c2d5c37fb62f7a7c9
capellinx <capellaaa7@gmail.com>
2023-08-04 01:31:35 -0300

    add responsive tablet and desktop

---
+++ main.js
@@ -0,0 +1,40 @@
+window.sr = ScrollReveal({ reset: true})
+
+sr.reveal('.experience', { 
+    distance: '250px', 
+    duration: 2500, 
+    viewFactor: 0.7
+    }, 
+)
+
+sr.reveal('.begin', { 
+    origin: 'top', 
+    distance: "250px", 
+    duration: 2500, 
+    viewFactor: 0.7 
+    }, 
+)
+sr.reveal('.projects', { 
+    distance: "250px", 
+    duration: 2500, 
+    viewFactor: 0.5 
+    }, 
+)
+
+sr.reveal('.service', { 
+    distance: "250px", 
+    duration: 2500, 
+    viewFactor: 0.5
+    }, 
+)
+
+sr.reveal('.skills', { 
+    distance: "50px", 
+    duration: 2500, 
+    viewFactor: 0.5
+    }, 
+)
+
+
+
+
