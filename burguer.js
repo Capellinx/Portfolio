10fb067cdcb6b2369ab95d2122075cbbc75f5159
capellinx <capellaaa7@gmail.com>
2023-08-05 16:17:44 -0300

    last update before dark mode

--- burguer.js
+++ burguer.js
@@ -1,33 +1,3 @@
-window.sr = ScrollReveal({ reset: true})
-
-sr.reveal('.experience', { 
-    distance: '250px', 
-    duration: 2500, 
-    viewFactor: 0.5
-    }, 
-)
-
-sr.reveal('.projects', { 
-    distance: "250px", 
-    duration: 2500, 
-    viewFactor: 0.5 
-    }, 
-)
-
-sr.reveal('.service', { 
-    distance: "250px", 
-    duration: 2500, 
-    viewFactor: 0.5
-    }, 
-)
-
-sr.reveal('.skills', { 
-    distance: "50px", 
-    duration: 2500, 
-    viewFactor: 0.5
-    }, 
-)
-
 const mobileMenu = document.querySelector('.mobile-menu')
 const navList = document.querySelector('.nav-list')
 const navLinks = document.querySelectorAll('.nav-links')
---
+++ reveal.js
@@ -0,0 +1,78 @@
+window.sr = ScrollReveal({ reset: true})
+
+sr.reveal('.info', { 
+    distance: '250px', 
+    duration: 2500, 
+    origin: 'top',
+    reset: false,
+    }, 
+)
+sr.reveal('.link-reveal', { 
+    distance: '250px', 
+    duration: 2500, 
+    origin: 'left',
+    reset: false,
+    }, 
+)
+
+sr.reveal('.image', { 
+    distance: '250px', 
+    duration: 2500, 
+    origin: 'top',
+    reset: false,
+    }, 
+)
+sr.reveal('.title', { 
+    distance: '150px', 
+    duration: 2500, 
+    origin: 'top',
+    reset: false,
+    viewFactor: 0.5
+    
+    }, 
+)
+sr.reveal('.timeline', { 
+    distance: '250px', 
+    duration: 2500, 
+    origin: 'bottom',
+    reset: false,
+    viewFactor: 0.5
+    
+    }, 
+)
+sr.reveal('.cards', { 
+    distance: '250px', 
+    duration: 2500, 
+    origin: 'right',
+    reset: false,
+    viewFactor: 0.5
+    
+    }, 
+)
+sr.reveal('.link', { 
+    distance: '250px', 
+    duration: 2500, 
+    origin: 'left',
+    reset: false,
+    viewFactor: 0.5
+    
+    }, 
+)
+
+sr.reveal('.cards-service', { 
+    distance: "250px", 
+    duration: 2500, 
+    viewFactor: 0.5,
+    origin: 'left',
+    reset: false,
+    }, 
+)
+
+sr.reveal('.skills-content', { 
+    distance: "50px", 
+    duration: 2500, 
+    viewFactor: 0.5,
+    reset: false
+    }, 
+)
+
